46430f28cea76eb4c066655e86441d4b99ec2ae2
codingalwaysmessy <marcus.wallau@googlemail.com>
2018-12-06 13:17:57 +0100

    added remaining JUnit tests

--- Tests/de/umr/swt/oop/ue8/util/StringUtilTest.java
+++ Tests/de/umr/swt/oop/ue8/util/StringUtilTest.java
@@ -25,18 +25,45 @@ public class StringUtilTest {
         Assert.assertEquals("True Grit",
                 StringUtil.replace("True Grit", "R", "")
         );
-
-
-
-
-
     }
 
     @Test
     public void isPalindrome() {
+
+        Assert.assertEquals(true,
+                StringUtil.isPalindrome("o")
+        );
+
+        Assert.assertEquals(true,
+                StringUtil.isPalindrome("Ebbe")
+        );
+
+        Assert.assertEquals(false,
+                StringUtil.isPalindrome("Array")
+        );
+
+        Assert.assertEquals(false,
+                StringUtil.isPalindrome("Al lets Della call Ed 'Stella'.")
+        );
     }
 
     @Test
     public void isSorted() {
+
+        Assert.assertEquals(true,
+                StringUtil.isSorted("bijoux", true));
+
+        Assert.assertEquals(false,
+                StringUtil.isSorted("Racecar", true));
+
+        Assert.assertEquals(false,
+                StringUtil.isSorted("cbaZYX", false));
+
+        Assert.assertEquals(true,
+                StringUtil.isSorted("Spoonfeed", false));
+
+
+
+
     }
 }
